80bedf496695a6c1a645efc2440d4e23445773b4
AdanaSyaifullah <datebayo200116@gmail.com>
2021-11-06 22:49:08 +0700

    Add project files.

---
+++ RentalKendaraan/Models/KondisiKendaraan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace RentalKendaraan.Models
+{
+    public partial class KondisiKendaraan
+    {
+        public KondisiKendaraan()
+        {
+            Pengembalian = new HashSet<Pengembalian>();
+        }
+
+        public int IdKondisi { get; set; }
+        public string NamaKondisi { get; set; }
+
+        public virtual ICollection<Pengembalian> Pengembalian { get; set; }
+    }
+}
